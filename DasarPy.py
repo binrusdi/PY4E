95c5df52c4a6fb23caf50ded390965a660a77ae4
binrusdi <mrusdiana07@gmail.com>
2023-11-20 12:55:01 +0700

    first commit

---
+++ DasarPy.py
@@ -0,0 +1,13 @@
+""" Dasar Python """
+''' Operator Aritmatika '''
+# ** eksponen
+print(f"Ekponen\n 3 ** 5 = {3 ** 5}")
+
+# % modulus
+print(f"Modulus\n 20 % 2 = {20 % 2}")
+
+# // pembagian bilangan bulat
+print(f"Pembagian bilangan bulat\n 5 // 2 = {5 // 2} ")
+
+# / Divisi
+print(f"Divisi\n 5 / 2 = {5 / 2}")
