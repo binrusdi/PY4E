c209c8bddbba30d9f105888355c97e9b19b2dc1c
binrusdi <mrusdiana07@gmai.com>
2023-12-14 21:42:58 +0700

    muali

---
+++ String01.py
@@ -0,0 +1,8 @@
+# Mengiris String
+word = 'Python'
+print(word[0:]) # Python
+print(word[0:3]) # Pyt
+print(word[:len(word)]) # Python
+print(word[-len(word):]) # Python
+print(word[-6:]) # Python -> karena urutan akhir string adalah -1
+
