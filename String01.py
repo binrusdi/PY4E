dcffc029baa6a786d80ab94a8796a921f7c18001
binrusdi <mrusdiana07@gmail.com>
2023-12-15 09:42:03 +0700

    15 Desember 2023

--- String01.py
+++ String01.py
@@ -6,3 +6,11 @@ print(word[:len(word)]) # Python
 print(word[-len(word):]) # Python
 print(word[-6:]) # Python -> karena urutan akhir string adalah -1
 
+# Memodifikasi string
+upper_word = word.upper()
+print('Ini method upper() mengganti ke huruf besar ->', upper_word) # PYTHON
+lower_word = word.lower()
+print('Ini method lower() mengganti ke huruf kecil ->', lower_word)
+strip_word = ' python '
+result_strip_word = strip_word.strip()
+print('Ini method strip() menghapus spasi awal dan akhir ->',result_strip_word)
