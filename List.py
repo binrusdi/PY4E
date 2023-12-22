d075b41192ad4ce47fe4e22529472a7913b923a8
binrusdi <mrusdiana07@gmai.com>
2023-12-22 17:13:06 +0700

    22 des 23

--- List.py
+++ List.py
@@ -31,3 +31,17 @@ print(listCampuran[2], type(listCampuran[2]))
 # my_list = ['satu', 2, True]
 # result_info_item = info_item(my_list)
 # print(result_info_item)
+
+# Konstruktor List
+thisListNew = list(('ayam', 12, True))
+print(thisListNew, type(thisListNew))
+
+# Memeriksa item dengan 'in'
+listBarang = ['penghapus', 'pulpen']
+if 'pulpen' in listBarang:
+    print('yes, itu adalah alat untuk menulis')
+
+# Ubah daftar item dengan melihat index
+fruit = ['apple', 'mango', 'watermelon']
+fruit[1] = 'pinaple'
+print(fruit)
