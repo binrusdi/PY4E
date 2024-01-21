34e9f71932587713a8c770be41164c9ee1557523
binrusdi <mrusdiana07@gmail.com>
2024-01-21 16:49:22 +0700

    21

--- Regex.py
+++ Regex.py
@@ -13,11 +13,21 @@ else:
 
 # Fungsi pada Regex
 # findall <- Mengembalikan List yg berisi semua kecocokan
-# search <- Mengembalikan objek Match jika ada kecocokan di manapun dalam string
+# search <- Mengembalikan objek yg cocok jika ada kecocokan di manapun dalam string
 # split <- Mengembalikan list yang stringnya telah dipisahkan pada setiap kecocokan
 # sub <- Mengganti satu atau banyak kecocokan dengan string
 
 # Metakarakter ialah karakter dengan arti khusus
-# [] <-
+# [] <- mencari/menampilkan seperangkat karakter
 kurung_siku = re.findall("[c-m]", txt)
 print(kurung_siku)
+
+# garis miring terbalik <- memberi sinyal untuk urutan khusus
+teks_garing = "disini ada angka 75"
+garing = re.findall("\d", teks_garing)
+print(garing)  # ['7', '5']
+
+# . <- Mencari karakter apapun, kecuali garis baru
+teks_titik = "kamu sangat cantik"
+titik = re.findall("ca..ik", teks_titik)
+print(titik)
