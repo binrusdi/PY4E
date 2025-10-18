767211f81f1f1ce77b912d0b03bf8d3dd443976f
binrusdi <mrusdiana07@gmail.com>
2025-10-18 14:27:07 +0700

    update file try.py

--- Try.py
+++ Try.py
@@ -7,11 +7,3 @@ try:
 except:
     print("Variable x tidak didefenisikan")
 
-# Blok 'try' memungkinkan anda menguji blok kode untuk menemukan kesalahan
-# Blok 'except' memungkinkan anda untuk ,enangani kesalahan tersebut
-# Blok 'else' memungkinkan anda mengeksekusi kode ketika tidak ada kesalahan
-# Blok 'finally' mengeksekusi kode, terlepas dari hasil try dan except
-try:
-    print(x)
-except:
-    print("Variable x tidak didefenisikan")
